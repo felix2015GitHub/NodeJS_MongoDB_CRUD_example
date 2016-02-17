3f3e8eea730f5fd6494df4bb2c64824154e9f89d
felix <felixcheng0808@gmail.com>
2016-02-18 00:25:55 +0800

    - init node.js CRUD example.

---
+++ routes/index.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Felix' });
+});
+
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+
+    	var name = [];
+        var objKey = Object.keys(docs);
+      	objKey.forEach(function(objectid){
+        	var items = Object.keys(docs[objectid]);
+        	items.forEach(function(itemkey) {
+          		var itemvalue =docs[objectid][itemkey];
+          		console.log(objectid+': '+itemkey+' = '+itemvalue);
+          		if(itemkey == "username") {
+          			name.push(itemvalue);
+          		}
+        	})
+      	})
+      	console.log(name);
+      	res.render('userlist', {
+          "userlist" : name
+      	});
+    });
+});
+
+/* GET New User page. */
+router.get('/adduser', function(req, res) {
+    res.render('adduser', { title: 'Add New User' });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+ 
+    // Set our internal DB variable
+    var db = req.db;
+ 
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var Email = req.body.email;
+ 
+    // Set our collection
+    var collection = db.get('usercollection');
+ 
+    // Submit to the DB
+    collection.insert({
+        "username" : userName,
+        "email" : Email
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // If it worked, set the header so the address bar doesn't still say /adduser
+            res.location("userlist");
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
+});
+
+module.exports = router;
